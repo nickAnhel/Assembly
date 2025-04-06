7c6dd7d13ecf81e3c19b69dad23c14298f0b315e
nickAnhel <anhimovn1@gmail.com>
2025-04-06 23:15:04 +0300

    Fixed indents in HelloWorld.asm

--- HelloWorld/HelloWorld.asm
+++ HelloWorld/HelloWorld.asm
@@ -1,17 +1,17 @@
-global _start				; make _start label global
+global _start                   ; make _start label global
 
-section .data				; static data section
-message db "Hello, world!", 1		; store data
+section .data                   ; static data section
+message db "Hello, world!", 0   ; store data
 
-section .text				; code section
-_start:					; entrypoint
+section .text                   ; code section
+_start:                         ; entrypoint
 
-	mov rax, 1			; suscall 'write'
-	mov rdi, 1			; file descriptor
-	mov rsi, message		; data pointer
-	mov rdx, 13			; number bytes in data
-	syscall
+    mov rax, 1                  ; suscall 'write'
+    mov rdi, 1                  ; file descriptor
+    mov rsi, message            ; data pointer
+    mov rdx, 13                 ; number bytes in data
+    syscall
 
-	mov rax, 60 			; syscall 'exit'
-	mov rdi, 0			; exit code	
-	syscall	
+    mov rax, 60                 ; syscall 'exit'
+    mov rdi, 0                  ; exit code
+    syscall	
