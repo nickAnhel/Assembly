56638dfc30d5a62daf5232ef6960972e29c6264a
nickAnhel <anhimovn1@gmail.com>
2025-04-06 03:22:55 +0300

    Add HelloWorld

---
+++ HelloWorld/HelloWorld.asm
@@ -0,0 +1,17 @@
+global _start				; make _start label global
+
+section .data				; static data section
+message db "Hello, world!", 1		; store data
+
+section .text				; code section
+_start:					; entrypoint
+
+	mov rax, 1			; suscall 'write'
+	mov rdi, 1			; file descriptor
+	mov rsi, message		; data pointer
+	mov rdx, 13			; number bytes in data
+	syscall
+
+	mov rax, 60 			; syscall 'exit'
+	mov rdi, 0			; exit code	
+	syscall	
